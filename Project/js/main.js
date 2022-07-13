09f88b3cfb63aeb8e86423040c32cc764779c329
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2022-07-13 16:13:23 +0530

    new project

---
+++ Project/js/main.js
@@ -0,0 +1,17 @@
+//owl-carousel for blog
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+});
+
+//click to scroll top
+
+$('.move-up span').click(function(){
+    $('html, body').animate({
+        scrollTop:0
+    }, 1000);
+})
